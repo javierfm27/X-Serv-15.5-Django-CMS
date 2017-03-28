8ed3a94ad3dac732bebd2cbc2e1e8de4ddd9e218
javierfm27 <javitofm27@github.com>
2017-03-28 09:26:31 +0200

    Cambios realizados a partir de check.py

--- cms/views.py
+++ cms/views.py
@@ -3,13 +3,14 @@ from django.http import HttpResponse
 from django.http import HttpResponseNotFound
 from cms.models import Pages
 
-# Create your views here.
+
 def base(request, recurso):
     try:
         pagina = Pages.objects.get(name=recurso)
         return HttpResponse(pagina.page)
     except Pages.DoesNotExist:
-        return HttpResponseNotFound('La pagina no existe o ha introducido mal el recurso, introduzca [nombre]App')
+        return HttpResponseNotFound('La pagina no existe o ha introducido mal \
+        el recurso, introduzca [nombre]App')
 
 def pepito(request):
     htmlAnswer = "<p> Pepito Saluda </p>"
