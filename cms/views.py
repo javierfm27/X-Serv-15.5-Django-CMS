76323b608e7cf337ab3420cbae0482e2f8e30120
javierfm27 <javitofm27@github.com>
2017-03-28 09:22:39 +0200

    Ejercicio acabado

--- cms/views.py
+++ cms/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.http import HttpResponseNotFound
+from cms.models import Pages
 
 # Create your views here.
+def base(request, recurso):
+    try:
+        pagina = Pages.objects.get(name=recurso)
+        return HttpResponse(pagina.page)
+    except Pages.DoesNotExist:
+        return HttpResponseNotFound('La pagina no existe o ha introducido mal el recurso, introduzca [nombre]App')
+
+def pepito(request):
+    htmlAnswer = "<p> Pepito Saluda </p>"
+    return HttpResponse(htmlAnswer)
--- myproject/urls.py
+++ myproject/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from cms import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'myproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^pepito', views.pepito, name='Prueba de una pagina pedida llamada pepito guardada en la base'),
+    url(r'^(.+App$)', views.base, name='Pagina que accede a los datos'),
     url(r'^admin/', include(admin.site.urls)),
 )
